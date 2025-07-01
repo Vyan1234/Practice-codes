268b7571c666995569d7f4d9da24c98f3bb665eb
Vyan1234 <vyanagarwal.insg@gmail.com>
2025-07-01 09:56:36 +0800

    Update revison.py

--- revison.py
+++ revison.py
@@ -1,4 +1,4 @@
-# x = int(input("Type a number: "))
-# for j in range (x +1):
-    # a = x  - j
-    # print("⭐️"*(j+1))
+x = int(input("Type a number: "))
+for j in range (x +1):
+    a = x  - j
+    print("⭐️"*(j+1))
